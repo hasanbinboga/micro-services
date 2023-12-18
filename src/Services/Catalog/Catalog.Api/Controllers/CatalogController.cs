623acc220c77b7401585738369cbc38d7f4995ef
Hasan BİNBOĞA <hasanbinboga@gmail.com>
2023-12-18 03:30:39 +0300

    Catalog Controller init

---
+++ src/Services/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger) 
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+    }
+}
